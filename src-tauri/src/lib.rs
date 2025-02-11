09d38e28998819084368eb7b4a231eeb308670e9
MC_XiaoHei <xiaohei.xor7@outlook.com>
2025-02-11 21:35:15 +0800

    feat: 使用独立的窗口展示 Vue Devtools，引入 Tauri Devtools

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,8 @@
+use crate::context::generate_context;
 use tauri::plugin::TauriPlugin;
-use tauri::{Builder, Manager, Wry};
+use tauri::{App, Builder, LogicalPosition, Manager, WebviewBuilder, WebviewUrl, WindowBuilder, Wry};
 use utils::window::setup_window;
 use utils::window::vibrancy::VibrancyStateStore;
-use crate::context::generate_context;
 
 mod account;
 mod command;
@@ -25,10 +25,15 @@ macro_rules! add_plugins {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+	// This should be called as early in the execution of the app as possible
+	#[cfg(debug_assertions)]
+	let devtools = tauri_plugin_devtools::init();
 	let builder = add_plugins!(
 		// tauri builder
 		Builder::default(),
 		// plugins
+		#[cfg(all(debug_assertions, not(mobile)))]
+		devtools,
 		#[cfg(not(mobile))]
 		single_instance_plugin(),
 		#[cfg(not(mobile))]
@@ -38,11 +43,12 @@ pub fn run() {
 		tauri_plugin_shell::init(),
 		tauri_plugin_store::Builder::new().build(),
 		#[cfg(not(mobile))]
-		prevent_default_plugin(),
-		log_plugin()
+		prevent_default_plugin()
 	)
 	.manage(VibrancyStateStore::new())
 	.setup(|app| {
+		#[cfg(debug_assertions)]
+		open_vue_devtools(app);
 		let vibrancy_state = setup_window::setup_window(app).unwrap();
 		app.state::<VibrancyStateStore>().set(vibrancy_state);
 		Ok(())
@@ -50,12 +56,6 @@ pub fn run() {
 	command::invoke_handler(builder)
 		.run(generate_context())
 		.expect("error while running tauri application");
-}
-
-fn log_plugin() -> TauriPlugin<Wry> {
-	tauri_plugin_log::Builder::new()
-		.level(log::LevelFilter::Info)
-		.build()
 }
 
 fn prevent_default_plugin() -> TauriPlugin<Wry> {
@@ -77,4 +77,21 @@ fn single_instance_plugin() -> TauriPlugin<Wry> {
 			.expect("no main window")
 			.set_focus();
 	})
+}
+
+fn open_vue_devtools(app: &App) {
+	let window_id = "vue-devtools";
+	if let Ok(window) = WindowBuilder::new(app, window_id)
+		.title("Vue Devtools")
+		.build() {
+		let webview_builder = WebviewBuilder::new(
+			window_id,
+			WebviewUrl::App("/__devtools__/".parse().unwrap()),
+		);
+		let _ = window.add_child(
+			webview_builder,
+			LogicalPosition::new(0, 0),
+			window.inner_size().unwrap(),
+		);
+	}
 }
